66eb4a95b026ef12968800b793af25730d28a12f
Branislav Trajkov <branislav.trajkov00@gmail.com>
2022-11-04 15:27:28 +0100

    Zavrsen login

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<?php
+require "DBBroker.php";
+require "model/korisnik.php";
+
+session_start();
+
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $rezultat = Korisnik::login($username, $password, $conn);
+
+    if($rezultat->num_rows == 1){
+        echo "Uspesno logovanje";
+        $_SESSION['usersigned'] = "ulogovan";
+        $_SESSION['id'] = $rezultat->fetch_assoc()['id'];
+        header('Location: teams.php');
+        exit();
+    }else{
+        echo "<script>
+        alert('Neuspesna prijava! Proverite unete podatke!');
+        window.location.href='';  
+        </script>";
+        exit();
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,8 +37,8 @@
 <body>
     <div class="login-card">
       <h2>Login</h2>
-      <h3>Enter your credentials</h3>
-      <form class="login-form" method = "post">
+      <h3>Unesite podatke</h3>
+      <form class="login-form" method = "POST">
         <input type="text" placeholder="Username" name="username" required/>
         <input type="password" placeholder="Password" name="password" required/>
         <button type="submit">LOGIN</button>
--- model/korisnik.php
+++ model/korisnik.php
@@ -5,16 +5,17 @@ class Korisnik{
     public $username;
     public $password;
 
-    public function __construct($id = null, $username = null, $password = null){
-        $this->id = $id;
-        $this->username = $username;
-        $this->password = $password;
-    }
+public function __construct($id = null, $username = null, $password = null){
+    $this->id = $id;
+    $this->username = $username;
+    $this->password = $password;
+}
     
-    public static function login($username, $password, mysqli $conn){
-        $query = "select * from korisnik where name='".$username."' and password='".$password."' limit 1;";
-        return $conn->query($query);
-    }
+public static function login($username, $password, mysqli $conn){
+    $q = "select * from korisnik where username= '".$username."' and password ='".$password."' limit 1;";
+    
+    return $conn->query($q);
+}
 
 }
 
